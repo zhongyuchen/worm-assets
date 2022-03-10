1643ae4d8804a37ab5a59f4360883657460b81ba
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-10 14:06:00 +0800

    add rotated cuticle blender model

--- sim.py
+++ sim.py
@@ -43,7 +43,7 @@ def worm(xml_file, stl_file):
 
 
 if __name__ == '__main__':
-    xml_str = worm('swimmer.xml', 'cuticle.stl')
+    xml_str = worm('swimmer.xml', 'cuticle_rotated.stl')
     model = mujoco_py.load_model_from_xml(xml_str.decode('utf-8'))
     sim = mujoco_py.MjSim(model)
     viewer = mujoco_py.MjViewer(sim)
