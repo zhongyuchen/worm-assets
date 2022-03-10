8bdb94f7103c2f0a7bb9111c84aedf96e72b7573
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-10 12:32:45 +0800

    simulate mesh by modifying swimmer.xml

--- sim.py
+++ sim.py
@@ -1,9 +1,52 @@
+from lxml import etree
 import mujoco_py
+import os
 
 
-model = mujoco_py.load_model_from_path('assets/cuticle.xml')
-sim = mujoco_py.MjSim(model)
-viewer = mujoco_py.MjViewer(sim)
-while True:
-    viewer.render()
-    sim.step()
+def parse_xml(xml_file):
+    """ parse xml and clean it up """
+    if not xml_file.startswith('/'):
+        xml_file = os.path.join(os.path.dirname(__file__), 'assets', xml_file)
+    with open(xml_file, 'rb') as f:
+        xml_str = f.read()
+    mjcf = etree.fromstring(xml_str, parser=etree.XMLParser(remove_blank_text=True))
+    # plane: set pos z=0
+    geom = mjcf.find('worldbody/geom')
+    geom.set('pos', '0 0 0')
+    # torso: set pos z=0.1
+    body = mjcf.find('worldbody/body')
+    body.set('pos', '0 0 0.1')
+    # remove geom, joint, body
+    body.remove(body.find('geom'))
+    for joint in body.findall('joint'):
+        body.remove(joint)
+    body.remove(body.find('body'))
+    # remove actuator
+    mjcf.remove(mjcf.find('actuator'))
+    return mjcf
+
+
+def make_mesh(mjcf, stl_file):
+    if not stl_file.startswith('/'):
+        stl_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets', stl_file)
+    mesh = etree.Element('mesh', attrib={'name': 'cuticle', 'file': stl_file})
+    geom = etree.Element('geom', attrib={'type': 'mesh', 'mesh': 'cuticle'})
+    mjcf.find('asset').append(mesh)
+    mjcf.find('worldbody/body').append(geom)
+    return mjcf
+
+
+def worm(xml_file, stl_file):
+    mjcf = parse_xml(xml_file)
+    mjcf = make_mesh(mjcf, stl_file)
+    return etree.tostring(mjcf, pretty_print=True)
+
+
+if __name__ == '__main__':
+    xml_str = worm('swimmer.xml', 'cuticle.stl')
+    model = mujoco_py.load_model_from_xml(xml_str.decode('utf-8'))
+    sim = mujoco_py.MjSim(model)
+    viewer = mujoco_py.MjViewer(sim)
+    while True:
+        viewer.render()
+        sim.step()
