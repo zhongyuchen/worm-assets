2ad6b30dba4d53556a4859dc6c9365807f4997ef
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-19 17:38:37 +0800

    add xml_path() func

--- worm_3d/__init__.py
+++ worm_3d/__init__.py
@@ -4,3 +4,7 @@ import worm_3d.envs
 
 def stl_path(id):
     return os.path.join(os.path.dirname(__file__), 'envs', 'mujoco', 'assets', 'Sphere.{:03d}.stl'.format(id))
+
+
+def xml_path(xml_file):
+    return os.path.join(os.path.dirname(__file__), 'envs', 'mujoco', 'assets', xml_file)
