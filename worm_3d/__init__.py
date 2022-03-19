5a0d50503c54f6fb23b89592dcf2f9d42045aa1d
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-03-19 19:33:26 +0800

    get asset path with asset_path()

--- worm_3d/__init__.py
+++ worm_3d/__init__.py
@@ -2,9 +2,5 @@ import os
 import worm_3d.envs
 
 
-def stl_path(id):
-    return os.path.join(os.path.dirname(__file__), 'envs', 'mujoco', 'assets', 'Sphere.{:03d}.stl'.format(id))
-
-
-def xml_path(xml_file):
-    return os.path.join(os.path.dirname(__file__), 'envs', 'mujoco', 'assets', xml_file)
+def asset_path(filename):
+    return os.path.join(os.path.dirname(__file__), 'envs', 'mujoco', 'assets', filename)
