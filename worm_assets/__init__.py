c7d9b9d2405f6f4ebedfbebf50679fe8e628ae55
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2022-07-31 22:19:18 +0800

    connectome: connectome_path() don't need filename

--- worm_assets/__init__.py
+++ worm_assets/__init__.py
@@ -6,5 +6,7 @@ def asset_path(filename):
     return os.path.join(os.path.dirname(__file__), 'envs', 'mujoco', 'assets', filename)
 
 
-def connectome_path(filename):
-    return os.path.join(os.path.dirname(__file__), 'connectomes', filename)
+def connectome_path():
+    return os.path.join(
+        os.path.dirname(__file__), 'connectomes', 'SI 5 Connectome adjacency matrices, corrected July 2020.xlsx'
+    )
